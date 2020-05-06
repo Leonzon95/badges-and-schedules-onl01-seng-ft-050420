2437e575f562c130a5dc3e856c1740a29d79abd4
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 00:29:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,8 @@ end
 
 def assign_rooms(names)
   assigned = []
-  names.each_with_index { |item, index|
+  names.each_with_index do |item, index|
     assigned << "Hello, #{item}! You'll be assigned to room #{index +1}!"
-  }
+  end
   assigned
 end
